e0ff358da36b74cd0ef51f57459d57f5008e5d09
Tokyo <ahmed.tokyo1@gmail.com>
2025-05-24 18:29:17 +0100

    :sparkles: more problems

--- src/count-and-say/count-and-say.py
+++ src/count-and-say/count-and-say.py
@@ -6,12 +6,19 @@ class Solution:
             if val[i] == val[i-1]:
                 count+=1
             else:
-                res += [str(count), val[i - 1]]
+                res.extend(((str(count), val[i - 1])))
                 count=1
         # append last element - also handles 1 case
-        res += [str(count), val[-1]]
+        res.extend((str(count), val[-1]))
         return ''.join(res)
 
     def countAndSay(self, n: int) -> str:
-        if n == 1: return '1'
-        return self.rle(self.countAndSay(n-1))
+        # recursive
+        # if n == 1: return '1'
+        # return self.rle(self.countAndSay(n-1))
+        #     def countAndSay(self, n: int) -> str:
+        # iterative
+        result = "1"
+        for _ in range(n - 1):
+            result = self.rle(result)
+        return result
